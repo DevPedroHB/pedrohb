7093f478bd18e6e81449e9ae2890f7d3b6ac9834
DevPedroHB <pedroh.bergamo20@gmail.com>
2024-11-27 17:42:39 -0300

    ♻️ refactor: Simplificar index.ts com exports de schemas e types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,25 +1,2 @@
-export * from "./schemas/pagination-query-schema";
-
-export * from "./schemas/account/create-account-schema";
-export * from "./schemas/account/create-authenticator-schema";
-export * from "./schemas/account/create-session-schema";
-export * from "./schemas/account/create-verification-token-schema";
-export * from "./schemas/account/credential-id-param-schema";
-export * from "./schemas/account/email-param-schema";
-export * from "./schemas/account/identifier-token-param-schema";
-export * from "./schemas/account/provider-account-id-param-schema";
-export * from "./schemas/account/session-token-param-schema";
-export * from "./schemas/account/sign-up-schema";
-export * from "./schemas/account/update-authenticator-counter-schema";
-export * from "./schemas/account/update-session-schema";
-export * from "./schemas/account/update-user-schema";
-export * from "./schemas/account/user-id-param-schema";
-
-export * from "./schemas/notification/notification-id-param-schema";
-
-export * from "./types/account";
-export * from "./types/authenticator";
-export * from "./types/notification";
-export * from "./types/session";
-export * from "./types/user";
-export * from "./types/verification-token";
+export * from "./schemas";
+export * from "./types";
---
+++ packages/types/src/types/index.ts
@@ -0,0 +1,6 @@
+export * from "./account";
+export * from "./authenticator";
+export * from "./notification";
+export * from "./session";
+export * from "./user";
+export * from "./verification-token";
